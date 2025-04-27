56cd637f5a27fdbbf105922c5c30052a0fe74757
Andy Estevez <Andy.E.Estevez@gmail.com>
2025-04-27 19:25:44 -0400

    Main: Updated Widgets

--- App/main.py
+++ App/main.py
@@ -1,10 +1,15 @@
 from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtWidgets import QApplication, QMainWindow, QStackedWidget, QWidget, QPushButton, QVBoxLayout
 import sys
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+
+        self.setWindowTitle("HungryText")
+
+        self.central_widget = QStackedWidget()
+        self.setCentralWidget(self.central_widget)
 
         self.showMaximized()
 
