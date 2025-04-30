b180e9a94faef18956bbfcea7ad971faa7edaf73
Andy Estevez <Andy.E.Estevez@gmail.com>
2025-04-30 19:41:27 -0400

    Utility Widgets: Added Widgets + Defined HeaderText widget + Defined InputLabel widget + Defined InputWarningLabel widget + Defined InputField widget + Defined ActionLabel widget + Defined PrimaryButton widget

--- App/util_widgets.py
+++ App/util_widgets.py
@@ -1,9 +1,89 @@
-from PyQt6.QtWidgets import QWidget
+from PyQt6.QtWidgets import QWidget, QLabel, QLineEdit, QPushButton, QSizePolicy
+from PyQt6.QtGui import QCursor
 from PyQt6.QtCore import Qt
 
 class Page(QWidget):
     def __init__(self):
         super().__init__()
 
+        self.setStyleSheet("Page { background-color: white; }")
         self.setAttribute(Qt.WidgetAttribute.WA_StyledBackground, True)
-        self.setStyleSheet("background-color: white;")
+
+class HeaderText(QLabel):
+    def __init__(self, text: str):
+        super().__init__(text)
+
+        self.setStyleSheet(
+            f"{type(self).__name__} {{"
+            "color: black;"
+            "font-size: 40px;"
+            "font-weight: 550;"
+            "}"
+        )
+
+        self.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+class InputLabel(QLabel):
+    def __init__(self, text: str):
+        super().__init__(text)
+
+        self.setStyleSheet(
+            f"{type(self).__name__} {{"
+            "color: black;"
+            "font-size: 12px;"
+            "}"
+        )
+
+class InputWarningLabel(QLabel):
+    def __init__(self, text: str = None):
+        super().__init__(text)
+
+        self.setStyleSheet(
+            f"{type(self).__name__} {{"
+            "color: transparent;"
+            "font-size: 12px;"
+            "}"
+        )
+
+    def toggle_text(self, show_text: bool):
+        if show_text: 
+            self.setStyleSheet(f"{type(self).__name__} {{ color: red; }}")
+        else:
+            self.setStyleSheet(f"{type(self).__name__} {{ color: transparent; }}")
+
+class InputField(QLineEdit):
+    def __init__(self, text: str = None, is_sensitive = False):
+        super().__init__(text)
+
+        self.setStyleSheet(
+            f"{type(self).__name__} {{"
+            "background-color: rgb(240, 240, 240);"
+            "color: black;"
+            "font-size: 15px;"
+            "}"
+        )
+
+        if is_sensitive: self.setEchoMode(QLineEdit.EchoMode.Password)
+
+class ActionLabel(QPushButton):
+    def __init__(self, text: str):
+        super().__init__(text)
+
+        self.setStyleSheet(
+            f"{type(self).__name__} {{"
+            "background-color: transparent;"
+            "color: purple;"
+            "font-size: 12px;"
+            "text-align: left;"
+            "}"
+        )
+
+        self.setSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
+        self.setCursor(QCursor(Qt.CursorShape.PointingHandCursor))
+        self.setFlat(True)
+
+class PrimaryButton(QPushButton):
+    def __init__(self, text: str = None):
+        super().__init__(text)
+
+        self.setCursor(QCursor(Qt.CursorShape.PointingHandCursor))
