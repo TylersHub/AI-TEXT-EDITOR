9383e3a6691d3ad6f98fb9f3f3a66dd77e1d81f6
Andy Estevez <Andy.E.Estevez@gmail.com>
2025-05-02 07:57:43 -0400

    Sign In Page: Updated Logic + Added Signals: All three slotted in Main    * session_token_received    * navigate_to_home    * navigate_to_sign_up + Added Slots:    * on_forgot_password_click (placeholder)    * on_sign_in_click (WIP)       * Uses validate_email from Util_Functions       * Uses validate_password from Util_Functions    * on_sign_up_click

--- App/page_sign_in.py
+++ App/page_sign_in.py
@@ -1,7 +1,13 @@
 from PyQt6.QtWidgets import QVBoxLayout
+from PyQt6.QtCore import pyqtSignal
 from util_widgets import Page, HeaderText, InputLabel, InputField, InputWarningLabel, ActionLabel, PrimaryButton
+from util_functions import validate_email, validate_password
 
 class SignInPage(Page):
+    session_token_received = pyqtSignal(int)
+    navigate_to_home = pyqtSignal()
+    navigate_to_sign_up = pyqtSignal()
+
     def __init__(self):
         super().__init__()
 
@@ -36,16 +42,85 @@ class SignInPage(Page):
         self.central_layout.addWidget(self.password_warning_label)
 
         self.forgot_password_label = ActionLabel("Forgot password?")
+        self.forgot_password_label.clicked.connect(self.on_forgot_password_click)
         self.central_layout.addWidget(self.forgot_password_label)
 
         # Call-To-Action
 
         self.sign_in_button = PrimaryButton("Sign In")
+        self.sign_in_button.clicked.connect(self.on_sign_in_click)
         self.central_layout.addWidget(self.sign_in_button)
 
         self.sign_up_label = ActionLabel("Don't have an account?")
+        self.sign_up_label.clicked.connect(self.on_sign_up_click)
         self.central_layout.addWidget(self.sign_up_label)
+
+        # Form Submission Shortcuts
+
+        self.email_input.returnPressed.connect(self.sign_in_button.click)
+        self.password_input.returnPressed.connect(self.sign_in_button.click)
 
         # End Of Layout
 
         self.central_layout.addStretch()
+
+    def on_forgot_password_click(self):
+        pass
+
+    def on_sign_in_click(self):
+        email_input_text = self.email_input.text()
+        password_input_text = self.password_input.text()
+        invalid_input = False
+        incorrect_input = False
+
+        # Email Input Validation
+
+        is_email_input_valid, invalid_email_input_error = validate_email(email_input_text)
+
+        if is_email_input_valid == False:
+            if invalid_email_input_error == "EMPTY":
+                self.email_warning_label.setText("Please enter an email")
+            elif invalid_email_input_error == "INVALID":
+                self.email_warning_label.setText("Please enter a valid email")
+
+            self.email_warning_label.toggle_text(True)
+            invalid_input = True
+        else:
+            self.email_warning_label.toggle_text(False)
+
+        # Password Input Validation
+
+        is_password_input_valid, invalid_password_input_error = validate_password(password_input_text)
+
+        if is_password_input_valid == False:
+            if invalid_password_input_error == "EMPTY":
+                self.password_warning_label.setText("Please enter a password")
+            elif invalid_password_input_error == "INVALID":
+                self.password_warning_label.setText("Please enter a valid password")
+
+            self.password_warning_label.toggle_text(True)
+            invalid_input = True
+        else:
+            self.password_warning_label.toggle_text(False)
+
+        # Invalid Input Termination
+
+        if invalid_input:
+            return
+        
+        # Input Authentication
+
+        ##### ... API ENDPOINT ... #####
+
+        # Incorrect Input Termination
+
+        if incorrect_input:
+            return
+        
+        # On Success
+
+        self.session_token_received.emit(12345) # NOTE: Should pass Session Token in future
+        self.navigate_to_home.emit()
+        
+    def on_sign_up_click(self):
+        self.navigate_to_sign_up.emit()
