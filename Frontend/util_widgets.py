72c8b5cd571a04b9c8028bc7acf02018321487ad
Andy Estevez <Andy.E.Estevez@gmail.com>
2025-05-10 00:39:01 -0400

    Utility Widgets: Updated Styling + Added color palette + Updated sizing and spacing    * Uses 8-Point Grid System

--- Frontend/util_widgets.py
+++ Frontend/util_widgets.py
@@ -1,12 +1,19 @@
 from PyQt6.QtWidgets import QWidget, QLabel, QLineEdit, QPushButton, QSizePolicy
-from PyQt6.QtGui import QCursor
+from PyQt6.QtGui import QColor, QCursor
 from PyQt6.QtCore import Qt
+
+# Color Palette
+primary_color = QColor("#F8F8FF")
+secondary_color = QColor("#7D55C7")
+accent_color = QColor("#8e78a3")
+dark_text_color = QColor("#252525")
+light_text_color = QColor("#F8F8FF")
 
 class Page(QWidget):
     def __init__(self):
         super().__init__()
 
-        self.setStyleSheet("Page { background-color: white; }")
+        self.setStyleSheet(f"Page {{ background-color: {primary_color.name()}; }}")
         self.setAttribute(Qt.WidgetAttribute.WA_StyledBackground, True)
 
 class HeaderText(QLabel):
@@ -15,9 +22,9 @@ class HeaderText(QLabel):
 
         self.setStyleSheet(
             f"{type(self).__name__} {{"
-            "color: black;"
+            f"color: {dark_text_color.name()};"
             "font-size: 40px;"
-            "font-weight: 550;"
+            "font-weight: 600;"
             "}"
         )
 
@@ -29,7 +36,8 @@ class InputLabel(QLabel):
 
         self.setStyleSheet(
             f"{type(self).__name__} {{"
-            "color: black;"
+            f"color: {dark_text_color.name()};"
+            "padding-left: 2px;"
             "font-size: 12px;"
             "}"
         )
@@ -39,21 +47,29 @@ class InputWarningLabel(QLabel):
         super().__init__(text)
 
         self.is_text_visible = False
-
-        self.setStyleSheet(
-            f"{type(self).__name__} {{"
-            "color: transparent;"
-            "font-size: 12px;"
-            "}"
-        )
+        self.toggle_text(False)
 
     def toggle_text(self, show_text: bool):
         if show_text: 
             self.is_text_visible = True
-            self.setStyleSheet(f"{type(self).__name__} {{ color: red; }}")
+
+            self.setStyleSheet(
+                f"{type(self).__name__} {{"
+                "color: rgb(200, 0, 0);"
+                "padding-left: 2px;"
+                "font-size: 12px;"
+                "}"
+            )
         else:
             self.is_text_visible = False
-            self.setStyleSheet(f"{type(self).__name__} {{ color: transparent; }}")
+
+            self.setStyleSheet(
+                f"{type(self).__name__} {{"
+                "color: transparent;"
+                "padding-left: 2px;"
+                "font-size: 12px;"
+                "}"
+            )
 
 class InputField(QLineEdit):
     def __init__(self, text: str = None, is_sensitive = False):
@@ -61,9 +77,13 @@ class InputField(QLineEdit):
 
         self.setStyleSheet(
             f"{type(self).__name__} {{"
-            "background-color: rgb(240, 240, 240);"
-            "color: black;"
-            "font-size: 15px;"
+            f"color: {dark_text_color.name()};"
+            f"background-color: {primary_color.name()};"
+            f"border: 1px solid {dark_text_color.name()};"
+            "border-radius: 8px;"
+            "height: 32px;"
+            "padding: 0 4px;"
+            "font-size: 16px;"
             "}"
         )
 
@@ -75,9 +95,10 @@ class ActionLabel(QPushButton):
 
         self.setStyleSheet(
             f"{type(self).__name__} {{"
+            f"color: {accent_color.name()};"
             "background-color: transparent;"
-            "color: purple;"
             "font-size: 12px;"
+            "font-weight: 600;"
             "text-align: left;"
             "}"
         )
@@ -89,5 +110,16 @@ class ActionLabel(QPushButton):
 class PrimaryButton(QPushButton):
     def __init__(self, text: str = None):
         super().__init__(text)
+
+        self.setStyleSheet(
+            f"{type(self).__name__} {{"
+            f"color: {primary_color.name()};"
+            f"background-color: {accent_color.name()};"
+            "border-radius: 8px;"
+            "height: 32px;"
+            "font-size: 16px;"
+            "font-weight: 600;"
+            "}"
+        )
 
         self.setCursor(QCursor(Qt.CursorShape.PointingHandCursor))
