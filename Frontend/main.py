bb545f3efc245071d78aa777e4bf08c09ea8596f
Andy Estevez <Andy.E.Estevez@gmail.com>
2025-05-11 07:23:27 -0400

    Main: Updated Logic + Overhauled page mechanism    * Added support for dynamic page loading and unloading       * Required for account type handling       * Required for account security + Added Home page

--- Frontend/main.py
+++ Frontend/main.py
@@ -4,6 +4,7 @@ import sys
 
 from page_sign_in import SignInPage
 from page_sign_up import SignUpPage
+from page_home import HomePage
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -15,39 +16,108 @@ class MainWindow(QMainWindow):
         self.central_widget = QStackedWidget()
         self.setCentralWidget(self.central_widget)
 
-        # Page Loading
+        # Session Credentials
+
+        self.session_token = None
+        self.account_type = None
+
+        # Page Storage
 
         self.pages = {
             "SignIn": SignInPage(),
             "SignUp": SignUpPage(),
+            "Home": None,
         }
 
-        for page in self.pages.values():
-            self.central_widget.addWidget(page)
+        # Page Slots
 
-        # Session Token Storage
-
-        self.session_token = None
-
-        self.pages["SignIn"].session_token_received.connect(self.store_session_token)
-
-        # Page Navigation
-
-        self.pages["SignIn"].navigate_to_home.connect(lambda: self.switch_to_page("Home"))
+        self.pages["SignIn"].session_credentials_received.connect(self.store_session_credentials)
         self.pages["SignIn"].navigate_to_sign_up.connect(lambda: self.switch_to_page("SignUp"))
+        self.pages["SignIn"].navigate_to_home.connect(lambda: self.switch_to_page("Home"))
 
         self.pages["SignUp"].navigate_to_sign_in.connect(lambda: self.switch_to_page("SignIn"))
+
+        # Page Loading
+
+        self.central_widget.addWidget(self.pages["SignIn"])
+        self.central_widget.addWidget(self.pages["SignUp"])
+
+        ###########################
+        ###### TEMP: TESTING ######
+        # self.session_token = 12345
+        # self.account_type = "PAID"
+        # self.switch_to_page("Home")
+        ###### TEMP: TESTING ######
+        ###########################
 
         # End Of Setup
 
         self.showMaximized()
 
-    def store_session_token(self, token):
+    def store_session_credentials(self, token, acc_type):
         self.session_token = token
+        self.account_type = acc_type
+
+    def clear_session_credentials(self):
+        # API ENDPOINT #
+        # Tell Backend To Discard Session Token
+        # API ENDPOINT #
+
+        self.session_token = None
+        self.account_type = None
+
+    def authenticate_session_credentials(self) -> bool:
+        if not self.session_token or not self.account_type:
+            self.session_token = None
+            self.account_type = None
+
+            return False
+        
+        # API ENDPOINT #
+        # Ask Backend To Verify Session Token And Account Type
+        # API ENDPOINT #
+        
+        # TEMP
+        return True
+        # TEMP
+
+    def unload_page(self, page: str):
+        self.central_widget.removeWidget(self.pages[page])
+        self.pages[page].deleteLater()
+        self.pages[page] = None
+    
+    def load_page(self, page: str):
+        # Clear Pages And Redirect To Sign In If Invalid Credentials
+        if self.authenticate_session_credentials() == False:
+            for pg in self.pages:
+                if pg != "SignIn" and pg != "SignUp" and self.pages[pg] != None:
+                    self.unload_page(pg)
+
+            self.central_widget.setCurrentWidget(self.pages["SignIn"])
+        else:
+            # Unload Page If It Already Exists
+            if self.pages[page] != None:
+                self.unload_page(page)
+
+            if page == "Home":
+                self.pages[page] = HomePage(self.account_type)
+                self.pages[page].sign_out_requested.connect(self.clear_session_credentials)
+                self.pages[page].navigate_to_sign_in.connect(lambda: self.switch_to_page("SignIn"))
+
+            self.central_widget.addWidget(self.pages[page])
+            self.central_widget.setCurrentWidget(self.pages[page])
 
     def switch_to_page(self, page: str):
         if page in self.pages:
-            self.central_widget.setCurrentWidget(self.pages[page])
+            if page == "SignIn" or page == "SignUp":
+                self.central_widget.setCurrentWidget(self.pages[page])
+            else:
+                if self.account_type == None:
+                    print(f"Error (switch_to_page): Page '{page}' given, but no user type provided!")
+                elif self.account_type != "FREE" and self.account_type != "PAID" and self.account_type != "ADMIN":
+                    print(f"Error (switch_to_page): Page '{page}' given, but invalid user type '{self.account_type}' provided!")
+                else:
+                    self.load_page(page)
         else:
             print(f"Error (switch_to_page): Page '{page}' not found in Pages!")
 
